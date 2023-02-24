db65ef8d8bb93a2b187f5a1d0e9d1318edd7a5b6
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-02-23 19:18:46 -0500

    finished footer.

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const PRESIDENT_MAIL_22_23 = 'jamie.shin@mail.utoronto.ca';
 const VICE_PRESIDENT_MAIL_22_23 = 'chae.kang@mail.utoronto.ca';
 const SPONSER_DIRECTOR_MAIL_22_23 = 'seohyun.kang@mail.utoronto.ca';
 
+window.onload = () => {
+  updateFooterMailAddress(
+    PRESIDENT_MAIL_22_23,
+    VICE_PRESIDENT_MAIL_22_23,
+    SPONSER_DIRECTOR_MAIL_22_23
+  );
+
+  // updateAnchorRel();
+};
+
 /**
+ * Not Yet Implemented.
+ * @param {String} pageName the name of the page to be implemented.
+ */
+function navToPage(pageName) {
+  console.log(`NAVIGATION TO ${pageName} TO BE IMPLEMENTED`);
+}
+
+/**
+ * updates the footer mail address tag of given <id> have the link as its href attribute.
  * precondition: the query is anchor, i.e. has an href attr.
  */
 function updateMailAddress(id, email) {
@@ -17,10 +36,9 @@ function updateFooterMailAddress(president, vice_president, sponser_director) {
   updateMailAddress('sponser_director', sponser_director);
 }
 
-window.onload = () => {
-  updateFooterMailAddress(
-    PRESIDENT_MAIL_22_23,
-    VICE_PRESIDENT_MAIL_22_23,
-    SPONSER_DIRECTOR_MAIL_22_23
-  );
-};
+function updateAnchorRel() {
+  const anchors = document.querySelectorAll('a');
+  for (const anchor in anchors) {
+    anchor.rel = 'noopener noreferrer';
+  }
+}
