8701667a8c3c9b4b724580ab7eefd9ed5b34d3eb
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-04-05 16:47:30 -0400

    social-events page complete. scalability

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const PRESIDENT_MAIL_22_23 = 'jamie.shin@mail.utoronto.ca';
-const VICE_PRESIDENT_MAIL_22_23 = 'chae.kang@mail.utoronto.ca';
-const SPONSOR_DIRECTOR_MAIL_22_23 = 'seohyun.kang@mail.utoronto.ca';
+/* --- constants --- */
+const PRESIDENT_MAIL_23_24 = 'seohyun.kang@mail.utoronto.ca';
+const VICE_PRESIDENT_MAIL_23_24 = 'suye.han@mail.utoronto.ca';
+const ER_DIRECTOR_MAIL_23_24 = 'yujin.shim@mail.utoronto.ca';
 
 const home_sponsor_photo_left = [
   '/img/home/sponsor/jjin1.jpg',
@@ -20,25 +21,16 @@ const home_sponsor_photo_right = [
   '/img/home/sponsor/wooriart2.jpg',
 ];
 
-let sponsor_photo_index = 0;
+/* --- end of constants ---*/
 
 window.onload = () => {
   updateFooterMailAddress(
-    PRESIDENT_MAIL_22_23,
-    VICE_PRESIDENT_MAIL_22_23,
-    SPONSOR_DIRECTOR_MAIL_22_23
+    PRESIDENT_MAIL_23_24,
+    VICE_PRESIDENT_MAIL_23_24,
+    ER_DIRECTOR_MAIL_23_24
   );
-
-  // updateAnchorRel();
-  setInterval(() => {
-    console.log(sponsor_photo_index);
-    // update slide index
-    sponsor_photo_index++;
-    if (sponsor_photo_index > 5) sponsor_photo_index = 0;
-
-    // execute function
-    updateSponsorPhoto(sponsor_photo_index);
-  }, 5000);
+  repeatUpdateSponsor();
+  updateSocialEvents();
 };
 
 /**
@@ -52,32 +44,50 @@ function navToPage(pageName) {
 /**
  * updates the footer mail address tag of given <id> have the link as its href attribute.
  * precondition: the query is anchor, i.e. has an href attr.
+ * @param {String} president - the email address string of current president
+ * @param {String} vice_president - that of current vice_president
+ * @param {String} er_director - that of current ER director
  */
-function updateMailAddress(id, email) {
+function updateFooterMailAddress(president, vice_president, er_director) {
+  updateMailAddress('president', president);
+  updateMailAddress('vice_president', vice_president);
+  updateMailAddress('sponsor_director', er_director);
+}
+
+const updateMailAddress = (id, email) => {
   const anchor = document.getElementById(id);
   anchor.textContent = email;
   anchor.href = 'mailto:' + email;
-}
-
-const updateFooterMailAddress = (
-  president,
-  vice_president,
-  sponsor_director
-) => {
-  updateMailAddress('president', president);
-  updateMailAddress('vice_president', vice_president);
-  updateMailAddress('sponsor_director', sponsor_director);
 };
 
-function updateAnchorRel() {
+const updateAnchorRel = () => {
   const anchors = document.querySelectorAll('a');
   for (const anchor in anchors) {
     anchor.rel = 'noopener noreferrer';
   }
+};
+
+let sponsor_photo_index = 0;
+
+/**
+ * Repetitively updates sponsor images in the main page.
+ * Update `home_sponsor_photo_left`and `home_sponsor_photo_right` for changing images.
+ */
+function repeatUpdateSponsor() {
+  setInterval(() => {
+    console.log(sponsor_photo_index);
+    // update slide index
+    sponsor_photo_index++;
+    if (sponsor_photo_index > 5) sponsor_photo_index = 0;
+
+    // execute function
+    updateSponsorPhoto(sponsor_photo_index);
+  }, 5000);
 }
 
-function updateSponsorPhoto(index) {
-  const imgList = document.querySelectorAll('.sponsor-photos');
+const updateSponsorPhoto = index => {
+  const imgList = document.querySelectorAll('img.sponsor-photos');
+  if (!imgList || !imgList[0]?.src) return;
 
   // old image: hide
   // new image: change src, reveal
@@ -103,11 +113,72 @@ function updateSponsorPhoto(index) {
     updateAnimationName(imgList[0], 'img-reveal');
     updateAnimationName(imgList[2], 'img-reveal');
   }
-}
+};
 
 const updateAnimationName = (element, animationName) => {
+  if (!element) return;
+
   element.style.animationName = '';
   element.style.animationName = animationName;
+};
+
+/**
+ * Using `makeSocialEventDiv(eventDict)`, construct event items based on social-events.json and update the DOM.
+ */
+function updateSocialEvents() {
+  const targetElement = document.getElementById('social');
+
+  // Gets data from social-events.json
+  fetch('./events/social-events.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      targetElement.append(
+        ...data.Events.slice(1).map(eventDict => makeSocialEventDiv(eventDict))
+      );
+    });
+}
+
+/**
+ * Make an HTML element that contains info about a single social event
+ * ```{HTML}
+ * <div> <!-- this is eventWrapperDiv -->
+ *   <img src={eventDict.image}/>
+ *   <div> <!-- this is miniDiv -->
+ *     <h2>{eventDict.name}</h2> <!-- this is name -->
+ *     <p>{eventDict.explanation}</p> <!-- this is exp -->
+ *   </div>
+ * </div>
+ * ```
+ *
+ * Preconditions:
+ *  @param {Object} eventDict is a valid event dictionary with three string key-value pairs: name, explanation, image. Refer to social-events.json for more details.
+ */
+const makeSocialEventDiv = eventDict => {
+  if (!eventDict) throw MediaError;
+
+  // create img element
+  const image = document.createElement('img');
+  image.src = eventDict.image;
+
+  // create h2 element
+  const name = document.createElement('h2');
+  name.append(eventDict.name);
+
+  // create p element
+  const exp = document.createElement('p');
+  exp.innerHTML = eventDict.explanation;
+
+  // create h2 + p wrapper div element, append.
+  const miniDiv = document.createElement('div');
+  miniDiv.append(name, exp);
+
+  // create parent div element
+  const eventWrapperDiv = document.createElement('div');
+  eventWrapperDiv.append(image, miniDiv);
+
+  return eventWrapperDiv;
 };
 
 // the scroll function that is triggered
