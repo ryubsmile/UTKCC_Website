fe92bd018efbf90511884ee1503f89c701e5fb71
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-04-04 22:24:15 -0400

    fixed typo sponser -> sponsor

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 const PRESIDENT_MAIL_22_23 = 'jamie.shin@mail.utoronto.ca';
 const VICE_PRESIDENT_MAIL_22_23 = 'chae.kang@mail.utoronto.ca';
-const SPONSER_DIRECTOR_MAIL_22_23 = 'seohyun.kang@mail.utoronto.ca';
+const SPONSOR_DIRECTOR_MAIL_22_23 = 'seohyun.kang@mail.utoronto.ca';
 
-const home_sponser_photo_left = [
-  '/img/home/sponser/jjin1.jpg',
-  '/img/home/sponser/kkar1.jpg',
-  '/img/home/sponser/lash1.jpg',
-  '/img/home/sponser/maple1.jpg',
-  '/img/home/sponser/sinjeon1.jpg',
-  '/img/home/sponser/wooriart1.jpg',
+const home_sponsor_photo_left = [
+  '/img/home/sponsor/jjin1.jpg',
+  '/img/home/sponsor/kkar1.jpg',
+  '/img/home/sponsor/lash1.jpg',
+  '/img/home/sponsor/maple1.jpg',
+  '/img/home/sponsor/sinjeon1.jpg',
+  '/img/home/sponsor/wooriart1.jpg',
 ];
 
-const home_sponser_photo_right = [
-  '/img/home/sponser/jjin2.jpg',
-  '/img/home/sponser/kkar2.jpg',
-  '/img/home/sponser/lash2.jpg',
-  '/img/home/sponser/maple2.jpg',
-  '/img/home/sponser/sinjeon2.jpg',
-  '/img/home/sponser/wooriart2.jpg',
+const home_sponsor_photo_right = [
+  '/img/home/sponsor/jjin2.jpg',
+  '/img/home/sponsor/kkar2.jpg',
+  '/img/home/sponsor/lash2.jpg',
+  '/img/home/sponsor/maple2.jpg',
+  '/img/home/sponsor/sinjeon2.jpg',
+  '/img/home/sponsor/wooriart2.jpg',
 ];
 
-let sponser_photo_index = 0;
+let sponsor_photo_index = 0;
 
 window.onload = () => {
   updateFooterMailAddress(
     PRESIDENT_MAIL_22_23,
     VICE_PRESIDENT_MAIL_22_23,
-    SPONSER_DIRECTOR_MAIL_22_23
+    SPONSOR_DIRECTOR_MAIL_22_23
   );
 
   // updateAnchorRel();
   setInterval(() => {
-    console.log(sponser_photo_index);
+    console.log(sponsor_photo_index);
     // update slide index
-    sponser_photo_index++;
-    if (sponser_photo_index > 5) sponser_photo_index = 0;
+    sponsor_photo_index++;
+    if (sponsor_photo_index > 5) sponsor_photo_index = 0;
 
     // execute function
-    updateSponserPhoto(sponser_photo_index);
+    updateSponsorPhoto(sponsor_photo_index);
   }, 5000);
 };
 
@@ -62,11 +62,11 @@ function updateMailAddress(id, email) {
 const updateFooterMailAddress = (
   president,
   vice_president,
-  sponser_director
+  sponsor_director
 ) => {
   updateMailAddress('president', president);
   updateMailAddress('vice_president', vice_president);
-  updateMailAddress('sponser_director', sponser_director);
+  updateMailAddress('sponsor_director', sponsor_director);
 };
 
 function updateAnchorRel() {
@@ -76,8 +76,8 @@ function updateAnchorRel() {
   }
 }
 
-function updateSponserPhoto(index) {
-  const imgList = document.querySelectorAll('.sponser-photos');
+function updateSponsorPhoto(index) {
+  const imgList = document.querySelectorAll('.sponsor-photos');
 
   // old image: hide
   // new image: change src, reveal
@@ -87,8 +87,8 @@ function updateSponserPhoto(index) {
     updateAnimationName(imgList[0], 'img-hide');
     updateAnimationName(imgList[2], 'img-hide');
 
-    imgList[1].src = home_sponser_photo_left[index];
-    imgList[3].src = home_sponser_photo_right[index];
+    imgList[1].src = home_sponsor_photo_left[index];
+    imgList[3].src = home_sponsor_photo_right[index];
 
     updateAnimationName(imgList[1], 'img-reveal');
     updateAnimationName(imgList[3], 'img-reveal');
@@ -97,8 +97,8 @@ function updateSponserPhoto(index) {
     updateAnimationName(imgList[1], 'img-hide');
     updateAnimationName(imgList[3], 'img-hide');
 
-    imgList[0].src = home_sponser_photo_left[index];
-    imgList[2].src = home_sponser_photo_right[index];
+    imgList[0].src = home_sponsor_photo_left[index];
+    imgList[2].src = home_sponsor_photo_right[index];
 
     updateAnimationName(imgList[0], 'img-reveal');
     updateAnimationName(imgList[2], 'img-reveal');
